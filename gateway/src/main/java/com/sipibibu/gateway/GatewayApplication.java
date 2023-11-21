139b20ef0707b14dad55828085a9d88a466eb57e
Nthokar <zhirnov.vova5@gmail.com>
2023-11-21 17:54:14 +0500

    github actions
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions
    
    github actions

--- gateway/src/main/java/com/sipibibu/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sipibibu/gateway/GatewayApplication.java
@@ -1,15 +1,13 @@
-package com.sipibibu.aplhagramms.main;
+package com.sipibibu.gateway;
 
-import com.sipibibu.aplhagramms.main.infastructure.config.RsaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RsaProperties.class)
 public class GatewayApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
