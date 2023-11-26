98ca1a143923b3051faa0609f48ee5daa5eede86
Nthokar <zhirnov.vova5@gmail.com>
2023-11-26 14:21:32 +0500

    fix up! configs

--- gateway/src/main/java/com/sipibibu/gateway/Route.java
+++ gateway/src/main/java/com/sipibibu/gateway/Route.java
@@ -1,5 +1,6 @@
 package com.sipibibu.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,15 +8,22 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Route {
+    @Value("${services.auth}")
+    String authUrl;
+    @Value("${services.api}")
+    String apiUrl;
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
                         .path("/auth/**")
-                        .uri("http://localhost:8090"))
+//                        .uri("http://localhost:8090"))
+                        .uri(authUrl))
                 .route(p -> p
                         .path("/api/**")
-                        .uri("http://localhost:8012"))
+                        //.uri("http://localhost:8012"))
+                        .uri(apiUrl))
                 .build();
     }
 }
